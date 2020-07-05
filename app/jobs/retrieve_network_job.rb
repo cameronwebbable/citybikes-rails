1950accfaa692fe5a65c2af5882b5beb2ff60cfe
cameronwebbable <cwebb02@gmail.com>
2020-07-05 10:34:58 -0500

    minor adjustments on job + networks fixture

--- app/jobs/retrieve_network_job.rb
+++ app/jobs/retrieve_network_job.rb
@@ -2,8 +2,8 @@ class RetrieveNetworkJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # response = retrieve_citybike_networks
-    # parse_response response
+    response = retrieve_citybike_networks
+    load_networks response
   end
 
   private 
@@ -12,38 +12,39 @@ class RetrieveNetworkJob < ApplicationJob
     HTTParty.get('http://api.citybik.es/v2/networks')
   end
   
-  def parse_response response
+  def load_networks response
     code = response.code
-    body = response.parsed_response
-    Rails::logger.debug "hey"
-
-    populate_data networks
+    parsed_response = response.parsed_response
+    
+    # map attrs and use upsert
+    populate_data body['networks']
   end
 
   def populate_data networks
-    location = network['location']
-
-    company_data = network['company']
-    company_name = ''
-
-    if company_data.kind_of?(Array)
-      company_name = network['company']&.join(', ')
-    elsif company_data.kind_of?(String)
-      company_name = company_data
-    end
-
     networks.each { |network|
-      Rails::logger.debug "hey"
-      Rails::logger.debug network["id"]
-      # n = Network.new(city: location['city'],
-      #                company_name: company_name,
-      #                country: location['country'],
-      #                latitude: location['latitude'],
-      #                longitude: location['longitude'],
-      #                name: network.name
-      #               )
-      # n.id = network['id']
+      Rails::logger.debug network
+      location = network['location']
+
+      company_data = network['company']
+      company_name = ''
+  
+      if company_data.kind_of?(Array)
+        company_name = network['company']&.join(', ')
+      elsif company_data.kind_of?(String)
+        company_name = company_data
+      end
+
+      network_attr = 
+      n = Network.create(id: network['id'],
+                         city: location['city'],
+                         company_name: company_name,
+                         country: location['country'],
+                         latitude: location['latitude'],
+                         longitude: location['longitude'],
+                         name: network['name']
+                        )
     }
+
   end
 end
 
