3fef7da32c7ee2183607079e9bae72f5b6faf21d
Cameron Webb <cwebb02@gmail.com>
2020-07-05 11:23:41 -0500

    switch over from ‘test’ to ‘rspec’

--- app/jobs/retrieve_network_job.rb
+++ app/jobs/retrieve_network_job.rb
@@ -2,8 +2,7 @@ class RetrieveNetworkJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    response = retrieve_citybike_networks
-    load_networks response
+    handle_response retrieve_citybike_networks
   end
 
   private 
@@ -12,39 +11,41 @@ class RetrieveNetworkJob < ApplicationJob
     HTTParty.get('http://api.citybik.es/v2/networks')
   end
   
-  def load_networks response
+  def handle_response response
     code = response.code
     parsed_response = response.parsed_response
     
-    # map attrs and use upsert
-    populate_data body['networks']
+    if parsed_response.key? 'networks'
+      load_networks parsed_response['networks']
+    end
   end
 
-  def populate_data networks
-    networks.each { |network|
-      Rails::logger.debug network
+  def load_networks networks
+    mapped_networks = networks.map { |network|
+
       location = network['location']
 
       company_data = network['company']
       company_name = ''
-  
+
       if company_data.kind_of?(Array)
         company_name = network['company']&.join(', ')
       elsif company_data.kind_of?(String)
         company_name = company_data
       end
 
-      network_attr = 
-      n = Network.create(id: network['id'],
-                         city: location['city'],
-                         company_name: company_name,
-                         country: location['country'],
-                         latitude: location['latitude'],
-                         longitude: location['longitude'],
-                         name: network['name']
-                        )
+      {
+        id: network['id'],
+        city: location['city'],
+        company_name: company_name,
+        country: location['country'],
+        latitude: location['latitude'],
+        longitude: location['longitude'],
+        name: network['name'] 
+      }
     }
 
+    Network.upsert_all(mapped_networks, unique_by: [:id])
   end
 end
 
