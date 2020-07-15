aea8942f9217c5584c1869c2f3d8d345393ad38d
cameronwebbable <cwebb02@gmail.com>
2020-07-14 21:41:08 -0500

    Add html and json responses

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,11 @@
 class BikesController < ApplicationController
   def index
     @networks = Network.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @networks }
+    end
+
   end
 end
